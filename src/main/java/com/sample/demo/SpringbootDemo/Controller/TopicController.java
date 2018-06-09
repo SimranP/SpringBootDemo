9664daa1ca81fdb0052b693a53239ff6a0d51678
simran pal <simranpalmeerut@gmail.com>
2018-06-09 11:56:35 +0530

    Simran | Add basic webClient

--- src/main/java/com/sample/demo/SpringbootDemo/Controller/TopicController.java
+++ src/main/java/com/sample/demo/SpringbootDemo/Controller/TopicController.java
@@ -21,37 +21,38 @@ public class TopicController {
 
     @GetMapping("/topics")
     public Flux<Topic> getAllTopics() {
+        System.out.println("coming here");
         return topicService.getAllTopics();
     }
 
-    @GetMapping("/topics/{id}")
-    public Mono<ResponseEntity<Topic>> getTopicById(@PathVariable(value="id") String topicId) {
-        return topicService.getTopic(topicId)
-                .map(topic -> ResponseEntity.ok(topic))
-                .defaultIfEmpty(ResponseEntity.notFound().build());
-    }
-
-    @PostMapping("/topics")
-    public void createTopic(@RequestBody Topic topic) {
-        topicService.addTopic(topic);
-    }
-
-    @PutMapping("/topics/{id}")
-    public Mono<ResponseEntity<Topic>> updateTopic(@PathVariable(value="id") String topicId, @Valid @RequestBody Topic topic) {
-        return topicService.getTopic(topicId)
-                .flatMap(existingTopic -> {
-                    existingTopic.setId(topic.getId());
-                    existingTopic.setName(topic.getName());
-                    existingTopic.setDescription(topic.getDescription());
-                    return Mono.just(existingTopic);
-                })
-                .map(updatedTopic ->  new ResponseEntity<>(updatedTopic, HttpStatus.OK))
-                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
-
-    @GetMapping(value = "/stream/topics", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Topic> streamAllTopics() {
-        return topicService.getAllTopics();
-    }
+//    @GetMapping("/topics/{id}")
+//    public Mono<ResponseEntity<Topic>> getTopicById(@PathVariable(value="id") String topicId) {
+//        return topicService.getTopic(topicId)
+//                .map(topic -> ResponseEntity.ok(topic))
+//                .defaultIfEmpty(ResponseEntity.notFound().build());
+//    }
+//
+//    @PostMapping("/topics")
+//    public void createTopic(@RequestBody Topic topic) {
+//        topicService.addTopic(topic);
+//    }
+//
+//    @PutMapping("/topics/{id}")
+//    public Mono<ResponseEntity<Topic>> updateTopic(@PathVariable(value="id") String topicId, @Valid @RequestBody Topic topic) {
+//        return topicService.getTopic(topicId)
+//                .flatMap(existingTopic -> {
+//                    existingTopic.setId(topic.getId());
+//                    existingTopic.setName(topic.getName());
+//                    existingTopic.setDescription(topic.getDescription());
+//                    return Mono.just(existingTopic);
+//                })
+//                .map(updatedTopic ->  new ResponseEntity<>(updatedTopic, HttpStatus.OK))
+//                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+//    }
+//
+//    @GetMapping(value = "/stream/topics", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//    public Flux<Topic> streamAllTopics() {
+//        return topicService.getAllTopics();
+//    }
 
 }
--- src/main/java/com/sample/demo/SpringbootDemo/Service/TopicService.java
+++ src/main/java/com/sample/demo/SpringbootDemo/Service/TopicService.java
@@ -2,33 +2,30 @@ package com.sample.demo.SpringbootDemo.Service;
 
 import com.sample.demo.SpringbootDemo.Model.Topic;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 public class TopicService {
-    private Flux<Topic> topics = Flux.fromIterable(Arrays.asList(
-            new Topic("spring", "Spring Framework", "Spring Framework"),
-            new Topic("java", "Core Java", "Core Java Descr"),
-            new Topic("javascript", "Javascript", "Javascript Descr")
-    ));
+      private WebClient jsonClient = WebClient.create("http://localhost:3000");
+//    private Flux<Topic> topics = Flux.fromIterable(Arrays.asList(
+//            new Topic("spring", "Spring Framework", "Spring Framework"),
+//            new Topic("java", "Core Java", "Core Java Descr"),
+//            new Topic("javascript", "Javascript", "Javascript Descr")
+//    ));
 
     public Flux<Topic> getAllTopics() {
-        return topics;
+        return jsonClient.get().uri("/topics").retrieve().bodyToFlux(Topic.class);
     }
 
-    public Mono<Topic> getTopic(String id) {
-        return topics.filter(topic -> topic.getId().equals(id))
-                .next();
-    }
-
-    public Mono<Topic> addTopic(Topic topic) {
-        Mono<Topic> newTopic = Mono.just(topic);
-        topics = topics.concatWith(newTopic);
-        return newTopic;
-    }
+//    public Mono<Topic> getTopic(String id) {
+//        return topics.filter(topic -> topic.getId().equals(id))
+//                .next();
+//    }
+//
+//    public Mono<Topic> addTopic(Topic topic) {
+//        Mono<Topic> newTopic = Mono.just(topic);
+//        topics = topics.concatWith(newTopic);
+//        return newTopic;
+//    }
 }
